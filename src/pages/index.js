98c1900dab43307906a18860939031cd495c18f8
Amos Chan <ac@amoschan.com>
2020-02-04 18:12:55 -0500

    Initial commit of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,113 @@
 import React from "react";
+import ReactMarkdown from "react-markdown"
+import styled from "styled-components"
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+const Description = styled.div`
+  a {
+    border-bottom: 1px dotted currentColor;
+  }
+
+  p {
+    margin-top: 0.5rem;
+  }
+`
 
 function IndexPage() {
+  const overview = {
+    title: "Hire Me",
+    body: `
+I help small companies and entrepreneurs build their products from the
+ground up.  From feature development to leading a team of developers, I
+work with my clients to make their vision a reality. **Currently only
+available for 10-15 hours per week.**
+`
+  }
+
+  const experience = [
+    {
+      company: "Flexday",
+      title: "Consultant",
+      tags: ["swift", "react", "firebase"],
+      tenure: "2018 - present",
+      description: `
+Remote work is the future and Flexday provides its members with a network of
+[unique, fully-equipped coworking spaces](https://www.theglobeandmail.com/canada/toronto/article-toronto-startup-flexday-partners-with-restaurants-to-turn-empty-tables/).
+
+I lead the development of their iOS app, web app and help out on their backend.
+`
+    },
+    {
+      company: "Code Academy",
+      title: "Consultant",
+      tags: ["react", "rubyonrails", "graphql"],
+      tenure: "2018 - present",
+      description: `Working with a young entrepreneur, we’re building an <abbr title="Learning Management System">LMS</abbr> that’s primarily focused on the developing world.`
+    },
+    {
+      company: "VarageSale",
+      title: "Engineering Team Lead",
+      tags: ["rubyonrails", "swift"],
+      tenure: "2014 - 2019",
+      description: `
+Facebook Marketplace before Facebook Marketplace. I joined a small team working
+on its Rails app and saw our work lead to a
+[$34 million Series B funding](https://www.theglobeandmail.com/report-on-business/small-business/sb-money/varagesale-app-raises-34-million-from-silicon-valley-investors/article23772596/).
+
+I pivoted over to iOS development to eventually lead the iOS team until its
+[acquisition by VerticalScope](http://www.montrealintechnology.com/montreal-founded-varagesale-acquired-by-verticalscope/)
+where I became the engineering team lead responsible for VarageSale‘s
+iOS, Android and web products.
+`
+    }
+  ]
+
+  const footer = `
+For a full list of experience with technologies like Swift, Ruby on Rails,
+React, Ember and more, see [my profile on
+LinkedIn](https://www.linkedin.com/in/amoschan).
+`
+
   return (
-    <Layout>
-      <SEO
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-        title="Home"
-      />
-
-      <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block mx-auto w-1/2"
-          src={catAndHumanIllustration}
-        />
-
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Hey there! Welcome to your first Gatsby site.
+    <div className="container mx-auto px-2 max-w-xl text-gray-700">
+      <div className="flex flex-row items-center mt-5 mb-10">
+        <img src="/static/avatar.jpg" className="w-20 h-20 rounded-full" />
+        <div className="flex flex-col text-xl hover:text-indigo-500 ml-4 align-center">
+          <div>Hey! 👋</div>
+          <div>I&apos;m <span className="font-semibold">Amos Chan</span>.</div>
+        </div>
+      </div>
+      <div>
+        <h1 className="bg-indigo-500 text-gray-100 text-xs px-2 py-1 rounded uppercase tracking-wider inline">
+          {overview.title}
+        </h1>
+        <ReactMarkdown className="mt-2 text-lg sm:text-xl" source={overview.body} />
+      </div>
+      <div className="mt-8">
+        <h2 className="bg-indigo-300 text-gray-100 text-xs px-2 py-1 rounded uppercase tracking-wider inline">
+          Past Experience
         </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold no-underline text-gray-900"
-            href="https://tailwindcss.com/"
-          >
-            Tailwind
-          </a>
-          , a utility-first CSS framework.
-        </p>
-      </section>
-    </Layout>
+        {experience.map((job, index) => {
+          return (
+            <div key={index} className="mb-10 last:mb-10">
+              <h3 className="mt-2 font-bold text-xl font-serif">
+                {job.company}, <span className="italic text-gray-600 text-md">{job.title}</span> 
+              </h3>
+              <div className="text-sm text-gray-500 tracking-wide">{job.tenure}</div>
+              <Description>
+                <ReactMarkdown className="mt-2" source={job.description} escapeHtml={false} />
+              </Description>
+              <div className="mt-2">
+                {job.tags.map(t => <span className="bg-gray-400 text-white text-xs mr-2 px-2 py-1 rounded-full tracking-wide" key={t}>#{t}</span>)}
+              </div>
+            </div>
+          )
+        })}
+        <Description>
+          <ReactMarkdown className="my-10" source={footer} escapeHtml={false} />
+        </Description>
+      </div>
+      <div className="my-20 text-4xl text-center">👨‍💻</div>
+    </div>
   );
 }
 
