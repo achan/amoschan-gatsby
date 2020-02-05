45e88b396ff4ea7da53e665978f4bc56d63adbd8
Amos Chan <ac@amoschan.com>
2020-02-05 08:12:05 -0500

    Improve responsive styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -69,10 +69,10 @@ LinkedIn](https://www.linkedin.com/in/amoschan).
 `
 
   return (
-    <div className="container mx-auto px-2 max-w-xl text-gray-700">
+    <div className="container mx-auto px-2 max-w-xl md:max-w-2xl text-gray-700">
       <div className="flex flex-row items-center mt-5 mb-10">
         <img src={avatar} className="w-20 h-20 rounded-full" />
-        <div className="flex flex-col text-xl hover:text-indigo-500 ml-4 align-center">
+        <div className="flex flex-col text-xl md:text-2xl hover:text-indigo-500 ml-4 align-center">
           <div>Hey! 👋</div>
           <div>I&apos;m <span className="font-semibold">Amos Chan</span>.</div>
         </div>
@@ -81,7 +81,7 @@ LinkedIn](https://www.linkedin.com/in/amoschan).
         <h1 className="bg-indigo-500 text-gray-100 text-xs px-2 py-1 rounded uppercase tracking-wider inline">
           {overview.title}
         </h1>
-        <ReactMarkdown className="mt-2 text-lg sm:text-xl" source={overview.body} />
+        <ReactMarkdown className="mt-2 text-lg sm:text-xl md:text-2xl" source={overview.body} />
       </div>
       <div className="mt-8">
         <h2 className="bg-indigo-300 text-gray-100 text-xs px-2 py-1 rounded uppercase tracking-wider inline">
@@ -90,20 +90,20 @@ LinkedIn](https://www.linkedin.com/in/amoschan).
         {experience.map((job, index) => {
           return (
             <div key={index} className="mb-10 last:mb-10">
-              <h3 className="mt-2 font-bold text-xl font-serif">
-                {job.company}, <span className="italic text-gray-600 text-md">{job.title}</span> 
+              <h3 className="mt-2 font-bold text-xl md:text-2xl font-serif">
+                {job.company}, <span className="italic text-gray-600">{job.title}</span> 
               </h3>
-              <div className="text-sm text-gray-500 tracking-wide">{job.tenure}</div>
+              <div className="text-sm md:text-base text-gray-500 tracking-wide">{job.tenure}</div>
               <Description>
-                <ReactMarkdown className="mt-2" source={job.description} escapeHtml={false} />
+                <ReactMarkdown className="mt-2 md:text-lg" source={job.description} escapeHtml={false} />
               </Description>
               <div className="mt-2">
-                {job.tags.map(t => <span className="bg-gray-400 text-white text-xs mr-2 px-2 py-1 rounded-full tracking-wide" key={t}>#{t}</span>)}
+                {job.tags.map(t => <span className="bg-gray-400 text-white text-xs md:text-sm mr-2 px-2 py-1 rounded-full tracking-wide" key={t}>#{t}</span>)}
               </div>
             </div>
           )
         })}
-        <Description>
+        <Description className="text-sm md:text-base text-gray-500 italic">
           <ReactMarkdown className="my-10" source={footer} escapeHtml={false} />
         </Description>
       </div>
