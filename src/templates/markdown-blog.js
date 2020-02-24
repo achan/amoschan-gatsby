0b8226bf3f5c7d4b7cb95c80eea331020fe41a6b
Amos Chan <ac@amoschan.com>
2020-02-23 19:41:29 -0500

    Share blog styles

--- src/templates/markdown-blog.js
+++ src/templates/markdown-blog.js
@@ -4,14 +4,15 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import { useMarkdownAst } from "../common/use-markdown-ast"
 
-const Paragraph = ({ children }) => <p className="mb-3 last:mb-0 md:mb-6 lg:mb-8 md:text-lg lg:text-xl">{children}</p>
-const Pre = ({ children }) => <pre className="bg-gray-900 p-4 -mx-2 overflow-x-scroll mb-3 md:mb-6 lg:mb-8 text-sm md:text-base lg:text-lg">{children}</pre>
-const BlockQuote = ({ children }) => <blockquote className="border-dashed border-l-2 border-indigo-200 p-2 md:pl-6 mb-3 md:mb-6 lg:mb-8">{children}</blockquote>
+export const Paragraph = ({ children }) => <p className="mb-3 last:mb-0 md:mb-6 lg:mb-8 md:text-lg lg:text-xl">{children}</p>
+export const Pre = ({ children }) => <pre className="bg-gray-900 p-4 -mx-2 overflow-x-scroll mb-3 md:mb-6 lg:mb-8 text-sm md:text-base lg:text-lg">{children}</pre>
+export const Code = ({ children }) => <code className="px-2 py-1 font-mono">{children}</code>
+export const BlockQuote = ({ children }) => <blockquote className="border-dashed border-l-2 border-indigo-200 p-2 md:pl-6 mb-3 md:mb-6 lg:mb-8">{children}</blockquote>
 
 const Template = ({ data }) => {
   const { markdownRemark } = data
   const { frontmatter, htmlAst } = markdownRemark
-  const renderAst = useMarkdownAst({ components: { p: Paragraph, pre: Pre, blockquote: BlockQuote } })
+  const renderAst = useMarkdownAst({ components: { p: Paragraph, pre: Pre, blockquote: BlockQuote, code: Code } })
 
   const post = {
     link: frontmatter.external_link,
